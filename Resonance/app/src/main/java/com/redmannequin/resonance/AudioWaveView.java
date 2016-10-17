d3bf5ad062cdf272003ba5fc5fcfa4bb1bfeafe4
flavio <redmannequin.10@gmail.com>
2016-10-17 12:06:52 -0400

    start

--- Resonance/app/src/main/java/com/redmannequin/resonance/AudioWaveView.java
+++ Resonance/app/src/main/java/com/redmannequin/resonance/AudioWaveView.java
@@ -4,13 +4,19 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
-
 
 public class AudioWaveView extends View {
 
     private Paint paint;
+    private Paint waveBrush;
+
+
+    private byte[] mBytes;
+    private float[] mPoints;
+    private Rect mRect;
 
     public AudioWaveView(Context context) {
         super(context);
@@ -30,11 +36,39 @@ public class AudioWaveView extends View {
     private void init() {
         paint = new Paint();
         paint.setColor(Color.GREEN);
+
+        waveBrush = new Paint();
+        waveBrush.setColor(Color.BLUE);
+        waveBrush.setAntiAlias(true);
+
+        mRect = new Rect();
+        mBytes = null;
+    }
+
+    public void updateVisualizer(byte[] bytes) {
+        mBytes = bytes;
+        invalidate();
     }
 
     @Override
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
         canvas.drawRect(0,0,getWidth(),getHeight(), paint);
+
+        if (mBytes == null) {
+            return;
+        }
+        if (mPoints == null || mPoints.length < mBytes.length * 4) {
+            mPoints = new float[mBytes.length * 4];
+        }
+        mRect.set(0, 0, getWidth(), getHeight());
+        for (int i = 0; i < mBytes.length - 1; i++) {
+            mPoints[i * 4 + 0] = mRect.width() * i / (mBytes.length - 1);
+            mPoints[i * 4 + 1] = mRect.height() / 2 + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
+            mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
+            mPoints[i * 4 + 3] = mRect.height() / 2 + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2) / 128;
+        }
+        canvas.drawLines(mPoints, waveBrush);
     }
+
 }
