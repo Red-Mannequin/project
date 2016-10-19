12d095adfb5ee4ad4c5f8be0686bf229a5c23a4f
flavio <redmannequin.10@gmail.com>
2016-10-19 09:35:09 -0400

    added views

---
+++ Resonance/app/src/main/java/com/redmannequin/resonance/ProjectListView.java
@@ -0,0 +1,13 @@
+package com.redmannequin.resonance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_list_view);
+    }
+}
