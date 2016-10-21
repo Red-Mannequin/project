22d3bc6c5a651ca8ad3d015d45885d4addb5aca8
flavio <redmannequin.10@gmail.com>
2016-10-21 15:16:47 -0400

    demo backend

--- Resonance/app/src/main/java/com/redmannequin/resonance/MainMenueView.java
+++ Resonance/app/src/main/java/com/redmannequin/resonance/MainMenueView.java
@@ -3,38 +3,70 @@ package com.redmannequin.resonance;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.redmannequin.resonance.BackendTesting.Backend;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainMenueView extends AppCompatActivity {
+
+    private Button new_project_button;
+    private Button setting_button;
+    private Button load_project_button;
+
+    private Backend testing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_view);
 
-        final Button new_project_button = (Button) findViewById(R.id.newProjcet);
+        init();
+
+        new_project_button = (Button) findViewById(R.id.newProjcet);
         new_project_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TrackListView.class);
+                Intent intent = new Intent(getApplicationContext(), NewProjectView.class);
                 startActivity(intent);
             }
         });
 
-        final Button load_project_button = (Button) findViewById(R.id.loadProject);
+        load_project_button = (Button) findViewById(R.id.loadProject);
         load_project_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ProjectListView.class);
+                intent.putExtra("backend", testing);
                 startActivity(intent);
             }
         });
 
-        final Button setting_button = (Button) findViewById(R.id.setting);
+        setting_button = (Button) findViewById(R.id.setting);
         setting_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SettingsView.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void init() {
+        InputStream ins = getResources().openRawResource(R.raw.test);
+        String json = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(ins));
+        try {
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                json += line;
+            }
+            Log.w("Backend:json:", json);
+            testing = new Backend(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
