b95a1ba13bdce7637a5fa3deb1b992ae42a00524
flavio <redmannequin.10@gmail.com>
2016-10-23 17:53:21 -0400

    big commit

---
+++ Resonance/app/src/main/java/com/redmannequin/resonance/Backend/Backend.java
@@ -0,0 +1,93 @@
+package com.redmannequin.resonance.Backend;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Backend implements Parcelable {
+
+    private String jsonStr;
+    private ArrayList<Project> projects;
+
+    public Backend(String json) {
+        this.jsonStr = json;
+        this.projects = null;
+        init();
+    }
+
+    private void init() {
+        this.projects = new ArrayList<Project>();
+
+        try {
+            JSONObject root = new JSONObject(jsonStr);
+            JSONArray projects = root.optJSONArray("projects");
+            for (int i=0; i < projects.length(); ++i) {
+                JSONObject project = projects.getJSONObject(i);
+                Project newProject = new Project(project.optString("name").toString());
+                JSONArray tracks = project.optJSONArray("tracks");
+                for (int j=0; j < project.length(); ++j) {
+                    JSONObject track = tracks.getJSONObject(j);
+                    Track newTrack = new Track(track.optString("name").toString());
+                    newProject.add(newTrack);
+                }
+                this.projects.add(newProject);
+            }
+        } catch (JSONException e) {
+
+        }
+    }
+
+    public Project getProject(int i) {
+        return projects.get(i);
+    }
+
+    public void add(Project project) {
+        projects.add(project);
+    }
+
+    public ArrayList<String> getProjectList() {
+        ArrayList<String> list = new ArrayList<String>();
+        for (Project project : projects) {
+            list.add(project.getName());
+        }
+        return list;
+    }
+
+    public int getProjectListSize() {
+        return projects.size();
+    }
+
+
+    protected Backend(Parcel in) {
+        jsonStr = in.readString();
+        projects = in.readArrayList(Project.class.getClassLoader());
+    }
+
+    public static final Creator<Backend> CREATOR = new Creator<Backend>() {
+        @Override
+        public Backend createFromParcel(Parcel in) {
+            return new Backend(in);
+        }
+
+        @Override
+        public Backend[] newArray(int size) {
+            return new Backend[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(jsonStr);
+        dest.writeList(projects);
+    }
+}
