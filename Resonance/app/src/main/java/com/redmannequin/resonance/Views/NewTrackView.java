899421716e448533de2275a7820629dd8adcd7c3
flavio <redmannequin.10@gmail.com>
2016-11-05 01:40:17 -0400

    got saving record file working

--- Resonance/app/src/main/java/com/redmannequin/resonance/Views/NewTrackView.java
+++ Resonance/app/src/main/java/com/redmannequin/resonance/Views/NewTrackView.java
@@ -1,8 +1,11 @@
 package com.redmannequin.resonance.Views;
 
 import android.content.Intent;
+import android.os.Environment;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +15,12 @@ import com.redmannequin.resonance.Backend.Project;
 import com.redmannequin.resonance.Backend.Track;
 import com.redmannequin.resonance.R;
 
+import java.io.File;
+
 public class NewTrackView extends AppCompatActivity {
+
+    private static final int TRACK_VIEW_RETRUN = 0;
+    private static final int RECORD_RETURN = 1;
 
     private String trackName;
     private String trackAuthor;
@@ -55,9 +63,14 @@ public class NewTrackView extends AppCompatActivity {
     private void setListeners() {
 
         trackPathInput.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
+            public void onClick(View v) {
+                File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "Resonance" + File.separator + project.getName());
+                if (!path.exists()) path.mkdirs();
+                File file = new File(path, "test");
+                trackPathInput.setText(file.getPath());
                 Intent intent = new Intent(getApplicationContext(), RecordTrackView.class);
-                startActivityForResult(intent, 0);
+                intent.putExtra("path", file.getPath());
+                startActivity(intent);
             }
         });
 
@@ -73,7 +86,7 @@ public class NewTrackView extends AppCompatActivity {
                 intent.putExtra("trackID", 0);
                 intent.putExtra("projectID", projectID);
                 intent.putExtra("backend", backend);
-                startActivityForResult(intent, 0);
+                startActivityForResult(intent, TRACK_VIEW_RETRUN);
             }
         });
     }
@@ -82,10 +95,14 @@ public class NewTrackView extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK) {
-            backend = getIntent().getParcelableExtra("backend");
-            projectID = getIntent().getIntExtra("projectID", 0);
-            onBackPressed();
+        Log.w("blah", requestCode + " " + resultCode + RESULT_OK);
+        if (requestCode == TRACK_VIEW_RETRUN) {
+            Log.w("blah", "TRACK_VIEW_RETURN");
+            if (resultCode == RESULT_OK) {
+                backend = getIntent().getParcelableExtra("backend");
+                projectID = getIntent().getIntExtra("projectID", 0);
+                onBackPressed();
+            }
         }
     }
 
