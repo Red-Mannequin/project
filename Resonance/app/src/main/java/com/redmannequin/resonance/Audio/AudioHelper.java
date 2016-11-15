bb59e060f5f0f4ed9b5aa39ee0d54690683608a3
Flavio B <redmannequin.10@gmail.com>
2016-11-14 22:59:01 -0500

    LOL how did that even happen. removed android test folder

--- Resonance/app/src/main/java/com/redmannequin/resonance/Audio/AudioHelper.java
+++ Resonance/app/src/main/java/com/redmannequin/resonance/Audio/AudioHelper.java
@@ -6,8 +6,8 @@ public class AudioHelper {
     public static byte[] short2byte(short[] buffer) {
         byte temp[] = new byte[buffer.length*2];
         for (int i=0; i < buffer.length; ++i) {
-            temp[i*2] = (byte) (buffer[i] & 0xFF);
-            temp[(i*2)+1] =  (byte) (buffer[i] >> 8);
+            temp[i*2] = (byte)(buffer[i]&0xFF);
+            temp[(i*2)+1] = (byte)(buffer[i]>>8);
         }
         return temp;
     }
