89a7e8a9e6233d236d6999922663065f66186c64
flavio <redmannequin.10@gmail.com>
2016-11-16 16:39:11 -0500

    addef flanger

--- Resonance/app/src/main/java/com/redmannequin/resonance/Audio/AudioEffect.java
+++ Resonance/app/src/main/java/com/redmannequin/resonance/Audio/AudioEffect.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioProcessor;
-import be.tarsos.dsp.PitchShifter;
 import be.tarsos.dsp.effects.DelayEffect;
+import be.tarsos.dsp.effects.FlangerEffect;
 import be.tarsos.dsp.io.TarsosDSPAudioFormat;
 import be.tarsos.dsp.io.UniversalAudioInputStream;
 import be.tarsos.dsp.writer.WriterProcessor;
@@ -24,12 +24,15 @@ public class AudioEffect {
     private AudioDispatcher dispatcher;
 
     private RandomAccessFile source;
+    private long sourceLength;
+    private String path;
     private String newPath;
 
     private ArrayList<AudioProcessor> processors;
 
     public AudioEffect(Track track) {
         this.track = track;
+        sourceLength = 0;
         audioFormat = null;
         audioInputStream = null;
         dispatcher = null;
@@ -38,10 +41,12 @@ public class AudioEffect {
 
     public void init() {
         audioFormat = new TarsosDSPAudioFormat(track.getSampleRate(), 16, 1, true, false);
-        String path = track.getPath() + File.separator + track.getName() + ".pcm";
+        path = track.getPath() + File.separator + track.getName() + ".pcm";
         newPath = track.getPath() + File.separator + track.getName() + "_final.wav";
         try {
-            source = new RandomAccessFile(path, "rw");
+            source = new RandomAccessFile(path, "r");
+            sourceLength = source.length();
+            source.close();
             audioInputStream = new UniversalAudioInputStream(new FileInputStream(path), audioFormat);
             dispatcher = new AudioDispatcher(audioInputStream, 1024, 0);
         } catch(IOException e) {
@@ -51,14 +56,21 @@ public class AudioEffect {
 
     public void addDelayEffect(double length, double decay) {
         try {
-            source.seek(source.length());
-            int b = (int)((source.length()/(length*Config.FREQUENCY*2))*decay);
+            source = new RandomAccessFile(path, "rw");
+            source.seek(sourceLength);
+            int b = (int)((sourceLength/(length*Config.FREQUENCY*2))*decay);
             source.write(new byte[(int)(b+length+(length*decay)+1)*Config.FREQUENCY*2]);
             DelayEffect delayEffect = new DelayEffect(length, decay, Config.FREQUENCY);
             processors.add(delayEffect);
+            source.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void addFlangerEffect() {
+        FlangerEffect flangerEffect = new FlangerEffect(20/1000.0, 50/100.0, Config.FREQUENCY, 3/10.0);
+        processors.add(flangerEffect);
     }
 
     public void make() {
@@ -69,6 +81,9 @@ public class AudioEffect {
             dispatcher.addAudioProcessor(new WriterProcessor(new TarsosDSPAudioFormat(track.getSampleRate(), 16, 2, true, false), audio));
             dispatcher.run();
             dispatcher.stop();
+            source = new RandomAccessFile(path, "rw");
+            source.setLength(sourceLength);
+            source.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
